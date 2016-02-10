fbf58d83f12c2a621eca8466b7ceeee5c16733a0
Ryan Helms <ryan@ryanhelms.com>
2016-02-10 17:12:10 -0500

    Added AssemblyInfo information for build server integration

--- ServiceStack.Contrib.Features/ServiceStack.Contrib.Features.UptimeFeature.Tests/Properties/AssemblyInfo.cs
+++ ServiceStack.Contrib.Features/ServiceStack.Contrib.Features.UptimeFeature.Tests/Properties/AssemblyInfo.cs
@@ -1,36 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("ServiceStack.Contrib.Features.Uptime.Tests")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Uptime feature tests")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ServiceStack.Contrib.Features.Uptime.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("2c777a8b-30d8-4066-8275-c9cbbc134f6a")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.*")]
