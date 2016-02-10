2d62d71f7b739283da4d01ed8c033ffccefa7f9b
Ryan Helms <ryan@ryanhelms.com>
2016-02-10 15:22:48 -0500

    Added test base class to take care of AppHost instantiation.

--- ServiceStack.Contrib.TestBase/ServiceStack.Contrib.TestBase/AppHostTestBase.cs
+++ ServiceStack.Contrib.TestBase/ServiceStack.Contrib.TestBase/AppHostTestBase.cs
@@ -1,23 +1,18 @@
 ﻿using Funq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using ServiceStack.Contrib.Features.UptimeFeature.ServiceInterface;
-using ServiceStack.Data;
-using ServiceStack.OrmLite;
 using ServiceStack.Testing;
 
-namespace ServiceStack.Contrib.Features.Uptime.Tests
+namespace ServiceStack.Contrib.TestBase
 {
     public class AppHostTestBase
     {
-        internal ServiceStackHost AppHost;
-        internal Container Container;
+        protected internal ServiceStackHost AppHost;
+        protected internal Container Container;
 
         public AppHostTestBase()
         {
             AppHost = new BasicAppHost().Init();
             Container = AppHost.Container;
-
-            Container.Register(new UptimeService());
         }
         
         [TestCleanup]
