55ee937dff38fd64a96571594e426957368f85a3
Ryan Helms <ryan@ryanhelms.com>
2016-02-12 14:17:17 -0500

    Included missing files for project

---
+++ TestBase/NUnit/AppHostTestBase.cs
@@ -0,0 +1,24 @@
+﻿using Funq;
+using NUnit.Framework;
+using ServiceStack.Testing;
+
+namespace ServiceStack.Contrib.TestBase.NUnit
+{
+    public class AppHostTestBase
+    {
+        public ServiceStackHost AppHost;
+        public Container Container;
+
+        public AppHostTestBase()
+        {
+            AppHost = new BasicAppHost().Init();
+            Container = AppHost.Container;
+        }
+        
+        [OneTimeTearDown]
+        public void TestCleanup()
+        {
+            AppHost.Dispose();
+        }
+    }
+}
