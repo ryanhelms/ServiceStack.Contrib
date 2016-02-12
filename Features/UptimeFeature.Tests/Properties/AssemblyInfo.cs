8ffcc80663aadc4773b836dcefd874f4244b685f
Ryan Helms <ryan@ryanhelms.com>
2016-02-12 18:13:00 -0500

    .

--- Features/UptimeFeature.Tests/Properties/AssemblyInfo.cs
+++ Features/UptimeFeature.Tests/Properties/AssemblyInfo.cs
@@ -1,14 +1,36 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("ServiceStack.Contrib.Features.Uptime.Tests")]
-[assembly: AssemblyDescription("Uptime feature tests")]
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("UptimeFeature.Tests")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ServiceStack.Contrib.Features.Uptime.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("UptimeFeature.Tests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-[assembly: Guid("2c777a8b-30d8-4066-8275-c9cbbc134f6a")]
-[assembly: AssemblyVersion("0.0.*")]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("30b33ef9-e341-45ef-a1fe-a290c9f5cf93")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Features/UptimeFeature.Tests/UptimeFeatureTests.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using NUnit.Framework;
+using ServiceStack.Contrib.Features.UptimeFeature.ServiceInterface;
+using ServiceStack.Contrib.Testing.NUnit;
+
+namespace ServiceStack.Contrib.Features.Uptime.Tests
+{
+    [TestFixture]
+    public class UptimeServiceTests : AppHostTestBase
+    {
+        public DateTime OriginalStartedAt => AppHost.StartedAt;
+        public UptimeService SUT => new UptimeService();
+
+        [Test]
+        public void Should_return_valid_uptime_string_with_seconds()
+        {
+            // Arrange
+            AppHost.StartedAt = OriginalStartedAt.Subtract(new TimeSpan(0, 0, 0, 30));
+
+            // Act
+            var response = SUT.Any(new UptimeFeature.ServiceModel.Uptime());
+
+            // Assert
+            Assert.IsTrue(response.Duration.Contains(" Second"));
+            Assert.IsFalse(response.Duration.Contains(" Minute"));
+        }
+
+        [Test]
+        public void Should_return_valid_uptime_string_with_minutes()
+        {
+            // Arrange
+            AppHost.StartedAt = OriginalStartedAt.Subtract(new TimeSpan(0, 0, 1, 0));
+
+            // Act
+            var response = SUT.Any(new UptimeFeature.ServiceModel.Uptime());
+
+            // Assert
+            Assert.IsTrue(response.Duration.Contains(" Minute"));
+            Assert.IsFalse(response.Duration.Contains(" Hour"));
+        }
+
+        [Test]
+        public void Should_return_valid_uptime_string_with_hours()
+        {
+            // Arrange
+            AppHost.StartedAt = OriginalStartedAt.Subtract(new TimeSpan(0, 1, 0, 0));
+
+            // Act
+            var response = SUT.Any(new UptimeFeature.ServiceModel.Uptime());
+
+            // Assert
+            Assert.IsTrue(response.Duration.Contains(" Hour"));
+            Assert.IsFalse(response.Duration.Contains(" Day"));
+        }
+
+        [Test]
+        public void Should_return_valid_uptime_string_with_days()
+        {
+            // Arrange
+            AppHost.StartedAt = OriginalStartedAt.Subtract(new TimeSpan(1, 0, 0, 0));
+
+            // Act
+            var response = SUT.Any(new UptimeFeature.ServiceModel.Uptime());
+
+            // Assert
+            Assert.IsTrue(response.Duration.Contains(" Day"));
+        }
+    }
+}
