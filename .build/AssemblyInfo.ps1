029a28d16cb8b980442a723ceef1fb39ad07015a
Ryan Helms <ryan@ryanhelms.com>
2016-02-15 01:54:45 -0500

    Fixed the spacing issues in the new powershell scripts

--- .build/AssemblyInfo.ps1
+++ .build/AssemblyInfo.ps1
@@ -1,48 +1,54 @@
 
 function Help {
-    "Sets the AssemblyVersion and AssemblyFileVersion of AssemblyInfo.cs files`n"
-    ".\SetAssemblyVersion.ps1 [VersionNumber] -path [SearchPath]`n"
-    "   [VersionNumber]     The version number to set, for example: 1.1.9301.0"
-    "   [SearchPath]        The path to search for AssemblyInfo files.`n"
+
+	"Sets the AssemblyVersion and AssemblyFileVersion of AssemblyInfo.cs files`n"
+	".\SetAssemblyVersion.ps1 [VersionNumber] -path [SearchPath]`n"
+	"   [VersionNumber]     The version number to set, for example: 1.1.9301.0"
+	"   [SearchPath]        The path to search for AssemblyInfo files.`n"
 }
 
 function Update-SourceVersion {
-    [CmdletBinding(
-        SupportsShouldProcess=$true
-    )]
-    Param 
+
+	[CmdletBinding(
+		SupportsShouldProcess=$true
+	)]
+	
+	Param 
 	(	
 		[string]$file,
 		[string]$version
 	)
-    PROCESS {
+	
+	PROCESS {
 
 		$NewVersion = 'AssemblyVersion("' + $version + '")';
-	    $NewFileVersion = 'AssemblyFileVersion("' + $version + '")';
+		$NewFileVersion = 'AssemblyFileVersion("' + $version + '")';
 
 		$assemblyVersionPattern = 'AssemblyVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)'
-        $fileVersionPattern = 'AssemblyFileVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)'
-        $assemblyVersion = 'AssemblyVersion("' + $version + '")';
-        $fileVersion = 'AssemblyFileVersion("' + $version + '")';
-        
+		$fileVersionPattern = 'AssemblyFileVersion\("[0-9]+(\.([0-9]+|\*)){1,3}"\)'
+		$assemblyVersion = 'AssemblyVersion("' + $version + '")';
+		$fileVersion = 'AssemblyFileVersion("' + $version + '")';
+
 		if($file)
 		{
-	        (Get-Content $file) | ForEach-Object  { 
-	           % {$_ -replace $assemblyVersionPattern, $assemblyVersion } |
-	           % {$_ -replace $fileVersionPattern, $fileVersion }
-	        } | Out-File $file -encoding UTF8 -force
+			(Get-Content $file) | ForEach-Object  { 
+				% {$_ -replace $assemblyVersionPattern, $assemblyVersion } |
+				% {$_ -replace $fileVersionPattern, $fileVersion }
+			} | Out-File $file -encoding UTF8 -force
 		}
-
 	}
 }
 
 function Update-AllAssemblyInfoFiles {
-    [CmdletBinding(
-        SupportsShouldProcess=$true
-    )]
-    Param ($version)
-    PROCESS {
-		
+
+	[CmdletBinding(
+		SupportsShouldProcess=$true
+	)]
+	
+	Param ($version)
+	
+	PROCESS {
+
 		foreach ($file in "AssemblyInfo.cs", "AssemblyInfo.vb" ) 
 		{
 			get-childitem $path -recurse |? {$_.Name -eq $file} | ForEach-Object { Update-SourceVersion $file $version ; }
@@ -52,27 +58,31 @@ function Update-AllAssemblyInfoFiles {
 
 
 function Get-NewVersionNumber {
-    [CmdletBinding(
-        SupportsShouldProcess=$true
-    )]
-    Param ($BuildNumber)
-    PROCESS {
-		
+
+	[CmdletBinding(
+	SupportsShouldProcess=$true
+	)]
+
+	Param ($BuildNumber)
+
+	PROCESS {
+
 		$Segments = $BuildNumber.Split(".")
 
-	    $Major = $Segments[0]
+		$Major = $Segments[0]
 		$Minor = $Segments[1]
 
 		$TodaysDate = (Get-Date) -as [DateTime]
 		$BeginDate = (Get-Date -Date '1/1/2000') -as [DateTime]
 
-	    $Build =  ($TodaysDate - $BeginDate).Days
+		$Build =  ($TodaysDate - $BeginDate).Days
 
-	    $h = ([int](Get-Date -UFormat "%H") * 60 * 60)
-	    $m = ([int](Get-Date -UFormat "%M") * 60)
-	    $s = ([int](Get-Date -UFormat "%S"))
-	    $Revision = [math]::ceiling(($h + $m + $s) / 2)
+		$h = ([int](Get-Date -UFormat "%H") * 60 * 60)
+		$m = ([int](Get-Date -UFormat "%M") * 60)
+		$s = ([int](Get-Date -UFormat "%S"))
+		$Revision = [math]::ceiling(($h + $m + $s) / 2)
 
-	    return [string]::Format("{0}.{1}.{2}.{3}", $Major, $Minor, $Build, $Revision)
+		return [string]::Format("{0}.{1}.{2}.{3}", $Major, $Minor, $Build, $Revision)
+		
 	}
 }
